e4fd2a74fa9b686db27b11f58bafd85367631cf3
mira-xx <mohana2406@outlook.com>
2024-01-16 23:00:45 +0000

    Summative

---
+++ alittlelife.js
@@ -0,0 +1,4 @@
+function moreInfo() {
+    var z = document.getElementBy("moreInfo");
+    z.innerHTML = "Hardcover, 520 pages";
+}
---
+++ main.js
@@ -0,0 +1,16 @@
+function funcClick() {
+    //to recall the text
+    var x = document.getElementById("sub");
+    
+    //change the text 
+    x.innerHTML = "Thank You for Subscribing!";
+    var y = document.getElementById("sub2");
+    y.innerHTML = "";
+}
+
+function Link() {
+    window.location.href ="alittlelife.html";
+}
+function backhome() {
+    window.location.href ="formative.html";
+}
